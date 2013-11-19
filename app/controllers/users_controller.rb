983bd4fb9c305d01e44e500390c712274fa80910
Jason Wan <jwan823@gmail.com>
2013-11-18 17:54:19 -0800

    Generates user controller and view

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(secure_user_params)
+    if @user.save
+      redirect_to movies_path
+    else
+      render :new
+    end
+  end
+
+  protected
+
+  def secure_user_params
+    params.require(:user).permit(:firstname, :lastname, :email, :password, :password_confirmation)
+  end
+end
